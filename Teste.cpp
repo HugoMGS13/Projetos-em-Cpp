ff99c5c4236742d476c77dc73c5741760338a8ac
HugoMGS13 <127580302+HugoMGS13@users.noreply.github.com>
2024-03-27 15:57:44 -0300

    Update Teste.cpp

--- Teste.cpp
+++ Teste.cpp
@@ -2,9 +2,15 @@
 using namespace std; // Usando a biblioteca padrão de nomes do C++
 
 int main() { // Criando uma função main que retorna um inteiro
+	
 	setlocale(LC_ALL,"Portuguese"); // Usando a linguagem portuguesa no código
-	int num;
-	cin >> num;
+	
+	int num; // Criando a variável do tipo inteiro chamada "num"
+	
+	cout << "Digite um número" <<; // Dizendo para o usuário digitar um número
+	
+	cin >> num; // Dizendo que num irá receber dados do usuário, usando a keyword "cin"
+	
 	if (num % 2 == 0){
 		cout<< "Par" << endl;
 	}
