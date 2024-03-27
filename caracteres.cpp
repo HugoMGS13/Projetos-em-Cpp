b71c6a10a34b07f495cd7528e3c477f8c36744a8
HugoMGS13 <127580302+HugoMGS13@users.noreply.github.com>
2024-03-27 17:02:18 -0300

    Create caracteres.cpp

---
+++ caracteres.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    char n = 20; //Cria um caractere em ASCII "n" que representa o inteiro 20
+    cout << "Caractere em ASCII " << n << ", Inteiro " << (int) n;
+}
